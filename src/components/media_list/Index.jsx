e8e69a14533d5773fb0c3bcdd21923e571a56172
Dat Hien Mai <hiendat04@gmail.com>
2024-12-17 20:53:01 +0700

    load API data for MovieCard

--- src/components/media_list/Index.jsx
+++ src/components/media_list/Index.jsx
@@ -1,11 +1,27 @@
 import { useEffect, useState } from "react";
 import MovieCard from "./MovieCard";
 
+const TABS = [
+  {
+    id: "all",
+    name: "All",
+  },
+  {
+    id: "movie",
+    name: "Movie",
+  },
+  {
+    id: "tv",
+    name: "TV Show",
+  },
+];
+
 const MediaList = () => {
   const [mediaList, setMediaList] = useState([]);
+  const [activeTabId, setActiveTabId] = useState(TABS[0].id);
 
   useEffect(() => {
-    fetch("https://api.themoviedb.org/3/trending/all/day", {
+    fetch(`https://api.themoviedb.org/3/trending/${activeTabId}/day`, {
       method: "GET",
       headers: {
         accept: "application/json",
@@ -17,18 +33,30 @@ const MediaList = () => {
       const trendingMediaList = data.results.slice(0, 12);
       setMediaList(trendingMediaList);
     });
-  }, []);
+  }, [activeTabId]);
 
   return (
     <div className="bg-black px-8 py-10 text-[1.2vw] text-white">
       <div className="mb-6 flex items-center gap-4">
         <p className="text-[2vw] font-bold">Trending</p>
         <ul className="flex rounded border border-white">
-          <li className="cursor-pointer rounded bg-white px-2 py-1 text-black">
-            All
-          </li>
-          <li className="cursor-pointer rounded px-2 py-1">Movie</li>
-          <li className="cursor-pointer rounded px-2 py-1">TV Show</li>
+          {TABS.map((tab) => {
+            return (
+              <li
+                key={tab.id}
+                onClick={() => {
+                  setActiveTabId(tab.id);
+                }}
+                className={`cursor-pointer rounded px-2 py-1 ${
+                  tab.id === activeTabId
+                    ? "bg-white text-black"
+                    : ""
+                }`}
+              >
+                {tab.name}
+              </li>
+            );
+          })}
         </ul>
       </div>
       <div className="grid grid-cols-2 gap-4 sm:grid-cols-4 lg:grid-cols-6">
