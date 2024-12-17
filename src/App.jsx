9fe078eb7b9bc3de0f5dad20d79912f03d735c67
Dat Hien Mai <hiendat04@gmail.com>
2024-12-17 21:45:33 +0700

    refactor code and fix UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
-
 import Header from "./components/Header";
 import FeatureMovies from "./components/feature_movies/Index";
 import MediaList from "./components/media_list/Index";
+import { TOP_RATED_TABS, TRENDING_TABS } from "./libs/Constant";
 
 function App() {
   return (
     <div>
-      <Header/>
-      <FeatureMovies/>
-      <MediaList/>
+      <Header />
+      <FeatureMovies />
+      <MediaList title="Trending" tabs={TRENDING_TABS} />
+      <MediaList title="Top Rated" tabs={TOP_RATED_TABS} />
     </div>
   );
 }
 
 export default App;
+ 
--- src/components/media_list/Index.jsx
+++ src/components/media_list/Index.jsx
@@ -1,46 +1,34 @@
 import { useEffect, useState } from "react";
 import MovieCard from "./MovieCard";
 
-const TABS = [
-  {
-    id: "all",
-    name: "All",
-  },
-  {
-    id: "movie",
-    name: "Movie",
-  },
-  {
-    id: "tv",
-    name: "TV Show",
-  },
-];
-
-const MediaList = () => {
+const MediaList = ({ title, tabs }) => {
   const [mediaList, setMediaList] = useState([]);
-  const [activeTabId, setActiveTabId] = useState(TABS[0].id);
+  const [activeTabId, setActiveTabId] = useState(tabs[0]?.id);
 
   useEffect(() => {
-    fetch(`https://api.themoviedb.org/3/trending/${activeTabId}/day`, {
-      method: "GET",
-      headers: {
-        accept: "application/json",
-        Authorization:
-          "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmY2IzYWY4MzhmNTgxY2IyMTFlNmZlZWI5YjVhYWRkYyIsIm5iZiI6MTczNDMzOTQ5NC4yMTEsInN1YiI6IjY3NWZlYmE2OTZjZmRkYmYxOWNjZDJjZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.X784ao5bLyFRagD_CJ-o705pamHW1jvk74yYQ4WPNr4",
-      },
-    }).then(async (res) => {
-      const data = await res.json();
-      const trendingMediaList = data.results.slice(0, 12);
-      setMediaList(trendingMediaList);
-    });
-  }, [activeTabId]);
+    const url = tabs.find((tab) => tab.id === activeTabId)?.url;
+    if (url) {
+      fetch(url, {
+        method: "GET",
+        headers: {
+          accept: "application/json",
+          Authorization:
+            "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmY2IzYWY4MzhmNTgxY2IyMTFlNmZlZWI5YjVhYWRkYyIsIm5iZiI6MTczNDMzOTQ5NC4yMTEsInN1YiI6IjY3NWZlYmE2OTZjZmRkYmYxOWNjZDJjZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.X784ao5bLyFRagD_CJ-o705pamHW1jvk74yYQ4WPNr4",
+        },
+      }).then(async (res) => {
+        const data = await res.json();
+        const trendingMediaList = data.results.slice(0, 12);
+        setMediaList(trendingMediaList);
+      });
+    }
+  }, [activeTabId, tabs]);
 
   return (
     <div className="bg-black px-8 py-10 text-[1.2vw] text-white">
       <div className="mb-6 flex items-center gap-4">
-        <p className="text-[2vw] font-bold">Trending</p>
+        <p className="text-[2vw] font-bold">{title}</p>
         <ul className="flex rounded border border-white">
-          {TABS.map((tab) => {
+          {tabs.map((tab) => {
             return (
               <li
                 key={tab.id}
@@ -48,9 +36,7 @@ const MediaList = () => {
                   setActiveTabId(tab.id);
                 }}
                 className={`cursor-pointer rounded px-2 py-1 ${
-                  tab.id === activeTabId
-                    ? "bg-white text-black"
-                    : ""
+                  tab.id === activeTabId ? "bg-white text-black" : ""
                 }`}
               >
                 {tab.name}
@@ -59,7 +45,7 @@ const MediaList = () => {
           })}
         </ul>
       </div>
-      <div className="grid grid-cols-2 gap-4 sm:grid-cols-4 lg:grid-cols-6">
+      <div className="grid grid-cols-2 gap-4 lg:gap-8 sm:grid-cols-4 lg:grid-cols-6">
         {mediaList.map((media) => {
           return (
             <MovieCard
@@ -68,7 +54,7 @@ const MediaList = () => {
               releaseDate={media.release_date || media.first_air_date}
               poster={media.poster_path}
               point={media.vote_average}
-              mediaType={media.media_type}
+              mediaType={media.media_type || activeTabId}
             />
           );
         })}
