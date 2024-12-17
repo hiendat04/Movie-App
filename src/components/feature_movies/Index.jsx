6badffbea0e969139dac57eeeaf61c00d0482e4e
Dat Hien Mai <hiendat04@gmail.com>
2024-12-17 11:46:43 +0700

    load API for feature movies

--- src/components/feature_movies/Index.jsx
+++ src/components/feature_movies/Index.jsx
@@ -1,13 +1,31 @@
-
 import PaginateIndicator from "./PaginateIndicator";
 import Movie from "./Movie";
+import { useEffect, useState} from "react";
 
 const FeatureMovies = () => {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    fetch("https://api.themoviedb.org/3/movie/popular", {
+      method: "GET",
+      headers: {
+        accept: "application/json",
+        Authorization:
+          "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmY2IzYWY4MzhmNTgxY2IyMTFlNmZlZWI5YjVhYWRkYyIsIm5iZiI6MTczNDMzOTQ5NC4yMTEsInN1YiI6IjY3NWZlYmE2OTZjZmRkYmYxOWNjZDJjZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.X784ao5bLyFRagD_CJ-o705pamHW1jvk74yYQ4WPNr4",
+      },
+    }).then(async (res) => {
+      const data = await res.json();
+      console.log(data); 
+      setMovies(data.results); 
+    });
+  }, []); // Empty dependency array ensures this runs only once when the component mounts
+
   return (
     <div className="relative text-white">
-      <Movie />
+      {movies.length > 0 ? <Movie data={movies[0]} /> : <p>Loading...</p>}
       <PaginateIndicator />
     </div>
   );
 };
+
 export default FeatureMovies;
--- src/components/feature_movies/Movie.jsx
+++ src/components/feature_movies/Movie.jsx
@@ -1,32 +1,29 @@
 import { faPlay } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-const Movie = () => {
+const Movie = (props) => {
+  const {
+    data: { backdrop_path, title, release_date, overview },
+  } = props;
   return (
     <div>
       <img
-        src="https://wallpaperswide.com/download/game_of_thrones-wallpaper-2400x1350.jpg"
+        src={`https://image.tmdb.org/t/p/original/${backdrop_path}`}
         alt=""
         className="aspect-video brightness-50"
       />
       <div className="absolute bottom-[10%] left-8 w-1/2 sm:w-1/3">
-        <p className="mb-2 font-bold sm:text-[2vw]">Game of Thrones</p>
+        <p className="mb-2 font-bold sm:text-[2vw]">{title}</p>
         <div>
           <p className="mb-1 inline-block border border-gray-400 p-1 text-gray-400">
             PG13
           </p>
-          <p className="mt-2 text-[1.2vw]">2024-06-11</p>
+          <p className="mt-2 text-[1.2vw]">{release_date}</p>
         </div>
         <div>
           <div className="mt-4 hidden text-[1.2vw] sm:block">
             <p className="mb-2 font-bold">Overview</p>
-            <p>
-              Game of Thrones follows a large cast of characters and interwoven
-              story arcs. It is primarily set on the fictional continent of
-              Westeros, which is divided into the Seven Kingdoms and the lands
-              in the far North beyond “the Wall,” an enormous wall of fortified
-              ice
-            </p>
+            <p>{overview}</p>
           </div>
           <div className="mt-6">
             <button className="text-10 mr-3 rounded bg-white px-4 py-2 text-black lg:text-lg">
